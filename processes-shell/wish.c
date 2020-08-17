9653e5222f4e90a4b815696335fb254bece83432
wanghao18 <wanghao18@bj-m-213396b.local>
2020-08-17 17:45:48 +0800

    add batch mode

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -20,7 +20,76 @@ char *trim(char *s) {
     return s;
 }
 
+void exec_single(char *line){
+    // support at most 10 parameters
+    char *myargs[10];
+    int count = 0;
+    char *found;
+    while( (found = strsep(&line," ")) != NULL ){
+        if(count == 9){
+            write(STDERR_FILENO, error_message, strlen(error_message));
+            exit(1);
+        }
+
+        if(count == 0){
+            char path[256] = "/bin/";
+            strncat(path, trim(found), 250);
+            myargs[0] = strdup(path);
+            printf("%s ", myargs[0]);
+        } else {
+            // https://blog.csdn.net/aaajj/article/details/53426767?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param
+            // 大坑：数组参数不能有空格，换行等，否则死活执行不了。是因为输入最后一个参数后回车导致有换行符，必须trim
+            myargs[count] = strdup(trim(found));
+            printf("%s ", myargs[count]);
+        }
+        count++;
+    }
+    myargs[count] = NULL;
+    printf("\n");
+    
+    execv(myargs[0], myargs);
+}
+
+void exec_batch(char *file){
+    // support at most 10 parameters
+    FILE *fp = fopen(file, "r");
+    if (fp == NULL) {
+        write(STDERR_FILENO, error_message, strlen(error_message));
+        exit(1);
+    }
+    
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    int count = 0;
+    while((nread = getline(&line, &len, fp)) != -1) {
+        count++;
+        fprintf(stdout, "%d %s\n", count, line);
+    
+        int rc = fork();
+        if (rc < 0) {
+            // fork failed
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        } else if (rc == 0) {
+            exec_single(line);
+        } else {
+            // parent goes down this path (main)
+            wait(NULL);
+        }
+    }
+    free(line);
+    
+    fclose(fp);
+}
+
 int main(int argc, char *argv[]) {
+    // batch mode
+    if(argc >= 2){
+        fprintf(stdout, "batch mode: \n");
+        exec_batch(argv[1]);
+        exit(0);
+    }
     
     char *line = NULL;
     size_t len = 0;
@@ -43,33 +112,7 @@ int main(int argc, char *argv[]) {
             // close(STDOUT_FILENO);
             // open("./ls.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
             
-            // support at most 10 parameters
-            char *myargs[10];
-            int count = 0;
-            char *found;
-            while( (found = strsep(&line," ")) != NULL ){
-                if(count == 9){
-                    write(STDERR_FILENO, error_message, strlen(error_message));
-                    exit(1);
-                }
-
-                if(count == 0){
-                    char path[256] = "/bin/";
-                    strncat(path, trim(found), 250);
-                    myargs[0] = strdup(path);
-                    printf("%s ", myargs[0]);
-                } else {
-                    // https://blog.csdn.net/aaajj/article/details/53426767?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param
-                    // 大坑：数组参数不能有空格，换行等，否则死活执行不了。是因为输入最后一个参数后回车导致有换行符，必须trim
-                    myargs[count] = strdup(trim(found));
-                    printf("%s ", myargs[count]);
-                }
-                count++;
-            }
-            myargs[count] = NULL;
-            printf("\n");
-            
-            execv(myargs[0], myargs);
+            exec_single(line);
         } else {
             // parent goes down this path (main)
             wait(NULL);
