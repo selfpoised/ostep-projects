7fe79086bfda0165eb248f4b6acbed584817a938
wanghao18 <wanghao18@bj-m-213396b.local>
2020-08-18 15:33:09 +0800

    fix exit

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -11,6 +11,7 @@ const char error_message[30] = "An error has occurred\n";
 char *trim(char *s);
 void exec_single(char *line);
 void exec_batch(char *file);
+void my_exit();
 
 int main(int argc, char *argv[]) {
     // batch mode
@@ -27,11 +28,7 @@ int main(int argc, char *argv[]) {
     fprintf(stdout, "wish> ");
     while((nread = getline(&line, &len, stdin)) > 0) {
         if (strcmp(trim(line), "exit\n") == 0 || strcmp(trim(line), "exit") == 0) {
-            // use built-in exit
-            char *myargs[2];
-            myargs[0] = strdup("./exit");
-            myargs[1] = NULL;
-            execv(myargs[0], myargs);
+            my_exit();
         }
         
         // Parallel Commands: cmd1 & cmd2 args1 args2 & cmd3 args1
@@ -67,66 +64,86 @@ char *trim(char *s) {
 }
 
 void exec_single(char *line){
-    int rc = fork();
-    if (rc < 0) {
-        // fork failed
-        fprintf(stderr, "fork failed\n");
-        exit(1);
-    } else if (rc == 0) {
-        char *line_refine = trim(line);
-        
-        // support at most 10 parameters
-        char *myargs[10];
-        int count = 0;
-        char *found;
-        
-        while( (found = strsep(&line_refine," ")) != NULL ){
-            if(count == 9){
-                write(STDERR_FILENO, error_message, strlen(error_message));
-                exit(1);
-            }
-            
-            // 跳过拆分过程中可能的空格
-            if(strlen(trim(found)) == 0){
-                continue;
-            }
-
-            if(count == 0){
-                if(strcmp(trim(found), "cd") == 0){
-                    myargs[0] = strdup("cd");
-                } else {
-                    char path[256] = "/bin/";
-                    strncat(path, trim(found), 250);
-                    myargs[0] = strdup(path);
-                }
-            } else {
-                // redirection
-                if(strcmp(trim(found), ">") == 0){
-                    // The reason this redirection works is due to an assumption about how the operating
-                    // system manages file descriptors. Specifically, UNIX systems start looking for free file descriptors at zero.
-                    // In this case, STDOUT FILENO will be the first available one and thus get assigned when open() is called.
-                    // Subsequent writes by the child process to the standard output file descriptor, for example by routines such
-                    // as printf(), will then be routed transparently to the newly-opened file instead of the screen
-                    close(STDOUT_FILENO);
-                    
-                    found = strsep(&line_refine," ");
-                    open(trim(found), O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-                    
-                    break;
-                } else {
-                    // https://blog.csdn.net/aaajj/article/details/53426767?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param
-                    // 大坑：数组参数不能有空格，换行等，否则死活执行不了。是因为输入最后一个参数后回车导致有换行符，必须trim
-                    myargs[count] = strdup(trim(found));
-                    // printf("%s ", myargs[count]);
-                }
-            }
-            count++;
+    char *line_refine = trim(line);
+    
+    // support at most 10 parameters
+    char *myargs[10];
+    int count = 0;
+    char *found;
+    
+    while( (found = strsep(&line_refine," ")) != NULL ){
+        if(count == 9){
+            write(STDERR_FILENO, error_message, strlen(error_message));
+            exit(1);
         }
-        myargs[count] = NULL;
         
-        execv(myargs[0], myargs);
-    } else {
+        // 跳过拆分过程中可能的空格
+        if(strlen(trim(found)) == 0){
+            continue;
+        }
+
+        if(count == 0){
+            if(strcmp(trim(found), "cd") == 0 || strcmp(trim(found), "exit") == 0){
+                myargs[0] = strdup(trim(found));
+            } else {
+                char path[256] = "/bin/";
+                strncat(path, trim(found), 250);
+                myargs[0] = strdup(path);
+            }
+        } else {
+            // redirection
+            if(strcmp(trim(found), ">") == 0){
+                // The reason this redirection works is due to an assumption about how the operating
+                // system manages file descriptors. Specifically, UNIX systems start looking for free file descriptors at zero.
+                // In this case, STDOUT FILENO will be the first available one and thus get assigned when open() is called.
+                // Subsequent writes by the child process to the standard output file descriptor, for example by routines such
+                // as printf(), will then be routed transparently to the newly-opened file instead of the screen
+                close(STDOUT_FILENO);
+                
+                found = strsep(&line_refine," ");
+                open(trim(found), O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+                
+                break;
+            } else {
+                // https://blog.csdn.net/aaajj/article/details/53426767?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param
+                // 大坑：数组参数不能有空格，换行等，否则死活执行不了。是因为输入最后一个参数后回车导致有换行符，必须trim
+                myargs[count] = strdup(trim(found));
+            }
+        }
+        count++;
+    }
+    myargs[count] = NULL;
+    
+    if(strcmp(myargs[0], "cd") == 0){
+        // The current working directory of the parent has not changed,
+        // since the command was executed in the child. As a result,
+        // the cd command although successful, did not produce the result that we desired.
+        // Thus, to support cd we will have to implement it on our own. We also need to ensure that,
+        // if the command entered by the user is cd (or belongs to a list of pre-defined built-in commands),
+        // we will not fork the process at all. Instead, we will execute our implementation of cd (or any other built-in)
+        // and move on to wait for the next user input. For cd, thankfully we have the chdir function call available to us
+        // and using it is straightforward. It accepts the path as an argument and returns 0 upon success and -1 upon a failure
+        if (count == 1 || chdir(myargs[1]) < 0) {
+            write(STDERR_FILENO, error_message, strlen(error_message));
+        }
         return;
+    } else if(strcmp(myargs[0], "exit") == 0){
+        if (count > 1) {
+            write(STDERR_FILENO, error_message, strlen(error_message));
+        } else {
+            my_exit();
+        }
+    }else {
+        int rc = fork();
+        if (rc < 0) {
+            // fork failed
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        } else if (rc == 0) {
+            execv(myargs[0], myargs);
+        } else {
+            return;
+        }
     }
 }
 
@@ -152,6 +169,13 @@ void exec_batch(char *file){
     free(line);
     
     fclose(fp);
+}
+
+void my_exit(){
+    char *myargs[2];
+    myargs[0] = strdup("./exit");
+    myargs[1] = NULL;
+    execv(myargs[0], myargs);
 }
 
 // gcc -o wish wish.c -Wall -Werror
