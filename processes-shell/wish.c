fb5ff958275dfbd8e0fcd00ebddd3e78409f483b
wanghao18 <wanghao18@bj-m-213396b.local>
2020-08-18 15:09:31 +0800

    mv fork in exec_single

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -26,15 +26,12 @@ int main(int argc, char *argv[]) {
     
     fprintf(stdout, "wish> ");
     while((nread = getline(&line, &len, stdin)) > 0) {
-        if (strcmp(trim(line), "exit\n") == 0 || strcmp(trim(line), "exit") == 0)
-        {
+        if (strcmp(trim(line), "exit\n") == 0 || strcmp(trim(line), "exit") == 0) {
             // use built-in exit
             char *myargs[2];
             myargs[0] = strdup("./exit");
             myargs[1] = NULL;
             execv(myargs[0], myargs);
-            
-            // exit(0);
         }
         
         // Parallel Commands: cmd1 & cmd2 args1 args2 & cmd3 args1
@@ -43,29 +40,10 @@ int main(int argc, char *argv[]) {
             char *found;
             while( (found = strsep(&line,"&")) != NULL ){
                 char *t = strdup(trim(found));
-                int rc = fork();
-                if (rc < 0) {
-                    // fork failed
-                    fprintf(stderr, "fork failed\n");
-                    exit(1);
-                } else if (rc == 0) {
-                    // exec_single(trim(found));
-                    exec_single(t);
-                }
+                exec_single(t);
             }
         } else {
-            int rc = fork();
-            if (rc < 0) {
-                // fork failed
-                fprintf(stderr, "fork failed\n");
-                exit(1);
-            } else if (rc == 0) {
-                // child: redirect standard output to a file
-                // close(STDOUT_FILENO);
-                // open("./ls.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-                
-                exec_single(line);
-            }
+            exec_single(line);
         }
         
         // parent goes down this path (main)
@@ -89,59 +67,67 @@ char *trim(char *s) {
 }
 
 void exec_single(char *line){
-    char *line_refine = trim(line);
-    
-    // support at most 10 parameters
-    char *myargs[10];
-    int count = 0;
-    char *found;
-    
-    while( (found = strsep(&line_refine," ")) != NULL ){
-        if(count == 9){
-            write(STDERR_FILENO, error_message, strlen(error_message));
-            exit(1);
-        }
+    int rc = fork();
+    if (rc < 0) {
+        // fork failed
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        char *line_refine = trim(line);
         
-        // 跳过拆分过程中可能的空格
-        if(strlen(trim(found)) == 0){
-            continue;
-        }
+        // support at most 10 parameters
+        char *myargs[10];
+        int count = 0;
+        char *found;
+        
+        while( (found = strsep(&line_refine," ")) != NULL ){
+            if(count == 9){
+                write(STDERR_FILENO, error_message, strlen(error_message));
+                exit(1);
+            }
+            
+            // 跳过拆分过程中可能的空格
+            if(strlen(trim(found)) == 0){
+                continue;
+            }
 
-        if(count == 0){
-            if(strcmp(trim(found), "cd") == 0){
-                myargs[0] = strdup("cd");
+            if(count == 0){
+                if(strcmp(trim(found), "cd") == 0){
+                    myargs[0] = strdup("cd");
+                } else {
+                    char path[256] = "/bin/";
+                    strncat(path, trim(found), 250);
+                    myargs[0] = strdup(path);
+                }
             } else {
-                char path[256] = "/bin/";
-                strncat(path, trim(found), 250);
-                myargs[0] = strdup(path);
+                // redirection
+                if(strcmp(trim(found), ">") == 0){
+                    // The reason this redirection works is due to an assumption about how the operating
+                    // system manages file descriptors. Specifically, UNIX systems start looking for free file descriptors at zero.
+                    // In this case, STDOUT FILENO will be the first available one and thus get assigned when open() is called.
+                    // Subsequent writes by the child process to the standard output file descriptor, for example by routines such
+                    // as printf(), will then be routed transparently to the newly-opened file instead of the screen
+                    close(STDOUT_FILENO);
+                    
+                    found = strsep(&line_refine," ");
+                    open(trim(found), O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+                    
+                    break;
+                } else {
+                    // https://blog.csdn.net/aaajj/article/details/53426767?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param
+                    // 大坑：数组参数不能有空格，换行等，否则死活执行不了。是因为输入最后一个参数后回车导致有换行符，必须trim
+                    myargs[count] = strdup(trim(found));
+                    // printf("%s ", myargs[count]);
+                }
             }
-        } else {
-            // redirection
-            if(strcmp(trim(found), ">") == 0){
-                // The reason this redirection works is due to an assumption about how the operating
-                // system manages file descriptors. Specifically, UNIX systems start looking for free file descriptors at zero.
-                // In this case, STDOUT FILENO will be the first available one and thus get assigned when open() is called.
-                // Subsequent writes by the child process to the standard output file descriptor, for example by routines such
-                // as printf(), will then be routed transparently to the newly-opened file instead of the screen
-                close(STDOUT_FILENO);
-                
-                found = strsep(&line_refine," ");
-                open(trim(found), O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-                
-                break;
-            } else {
-                // https://blog.csdn.net/aaajj/article/details/53426767?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.channel_param
-                // 大坑：数组参数不能有空格，换行等，否则死活执行不了。是因为输入最后一个参数后回车导致有换行符，必须trim
-                myargs[count] = strdup(trim(found));
-                // printf("%s ", myargs[count]);
-            }
+            count++;
         }
-        count++;
+        myargs[count] = NULL;
+        
+        execv(myargs[0], myargs);
+    } else {
+        return;
     }
-    myargs[count] = NULL;
-    // printf("\n");
-    
-    execv(myargs[0], myargs);
 }
 
 void exec_batch(char *file){
@@ -158,19 +144,10 @@ void exec_batch(char *file){
     int count = 0;
     while((nread = getline(&line, &len, fp)) != -1) {
         count++;
-        // fprintf(stdout, "%d %s\n", count, line);
-    
-        int rc = fork();
-        if (rc < 0) {
-            // fork failed
-            fprintf(stderr, "fork failed\n");
-            exit(1);
-        } else if (rc == 0) {
-            exec_single(line);
-        } else {
-            // parent goes down this path (main)
-            wait(NULL);
-        }
+        
+        exec_single(line);
+        
+        wait(NULL);
     }
     free(line);
     
