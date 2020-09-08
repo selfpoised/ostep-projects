326ba55503982d0c6d63a39e1f3241a2c83df9a9
wanghao18 <wanghao18@bj-m-213396b.local>
2020-09-09 01:47:52 +0800

    process incoming request using thread

--- concurrency-webserver/src/wserver.c
+++ concurrency-webserver/src/wserver.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include "request.h"
 #include "io_helper.h"
+#include <pthread.h>
+#include <stdlib.h> 
+
+#define Pthread_create(thread, attr, start_routine, arg) assert(pthread_create(thread, attr, start_routine, arg) == 0);
 
 char default_root[] = ".";
+char FIFO[] = "FIFO";
+char SFF[] = "SFF";
+
+void *run(void *p);
 
 //
 // ./wserver [-d <basedir>] [-p <portnum>]
@@ -12,8 +20,11 @@ int main(int argc, char *argv[])
 	int c;
 	char *root_dir = default_root;
 	int port = 10000;
+	int num_thread_pool_size = 1;
+	int queue_size = 1;
+	char *schedalg = NULL;
 
-	while ((c = getopt(argc, argv, "d:p:")) != -1)
+	while ((c = getopt(argc, argv, "d:p:t:b:s")) != -1)
 		switch (c)
 		{
 		case 'd':
@@ -22,8 +33,17 @@ int main(int argc, char *argv[])
 		case 'p':
 			port = atoi(optarg);
 			break;
+		case 't':
+			num_thread_pool_size = atoi(optarg);
+			break;
+		case 'b':
+			queue_size = atoi(optarg);
+			break;
+		case 's':
+			schedalg = optarg;
+			break;
 		default:
-			fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
+			fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffers] [-s schedalg]\n");
 			exit(1);
 		}
 
@@ -37,8 +57,21 @@ int main(int argc, char *argv[])
 		struct sockaddr_in client_addr;
 		int client_len = sizeof(client_addr);
 		int conn_fd = accept_or_die(listen_fd, (sockaddr_t *)&client_addr, (socklen_t *)&client_len);
-		request_handle(conn_fd);
-		close_or_die(conn_fd);
+
+		pthread_t p;
+		Pthread_create(&p, NULL, run, (void *) (long long) conn_fd);
+
+		// request_handle(conn_fd);
+		// close_or_die(conn_fd);
 	}
 	return 0;
+}
+
+void *run(void *arg) {
+	printf("\n####thread %li\n", (unsigned long int)pthread_self());
+    int conn_fd = (int) arg;
+	request_handle(conn_fd);
+	close_or_die(conn_fd);
+
+	return NULL;
 }
