87072b104fdef46ef7608eab65a0c92353d49bce
wanghao18 <wanghao18@bj-m-213396b.local>
2020-09-11 14:24:51 +0800

    implement pzip

--- concurrency-pzip/pzip.c
+++ concurrency-pzip/pzip.c
@@ -3,72 +3,206 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include "common_threads.h"
+#include "io_helper.h"
+#include <pthread.h>
 
-void
-wzip(char * c, long size)
+/**
+ * 本实现采用线程池处理输入文件方式，线程池竞争消费文件
+ * 更合理的方式也许是将文件切分为一定的片段，然后再进行处理
+ */
+
+// check file content or final zip output
+// od -c file
+// od -c t.z
+
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
+void *consume(void *arg);
+
+int file_count = 0;
+int count = 0;
+int done = 0;
+char **pArgv = NULL;
+
+void pzip(char *c, long size, FILE *fp)
 {
-    // printf("%s\n", c);
-    
     int i;
     int per_count = 1;
-    for(i = 0; i < size; i++){
-        if(i == (size-1)){
-            fwrite(&per_count, 4, 1, stdout);
-            printf("%c", c[i]);
+    for (i = 0; i < size; i++)
+    {
+        if (i == (size - 1))
+        {
+            fwrite(&per_count, 4, 1, fp);
+            fprintf(fp, "%c", c[i]);
             break;
         }
-    
-        if(c[i] != c[i+1]){
-            // printf("%d%c", per_count, c[i]);
-            fwrite(&per_count, 4, 1, stdout);
-            printf("%c", c[i]);
+
+        if (c[i] != c[i + 1])
+        {
+            fwrite(&per_count, 4, 1, fp);
+            fprintf(fp, "%c", c[i]);
             per_count = 1;
-        } else {
+        }
+        else
+        {
             per_count++;
         }
     }
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    if(argc <= 1){
+    pArgv = argv;
+
+    if (argc <= 1)
+    {
         printf("pzip: file1 [file2 ...]\n");
         exit(1);
     }
-    
-    clock_t tic = clock();
+
+    file_count = argc - 1;
+    count = 0;
 
     int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
-    fprintf( stderr, "This system has %d processors\n", num_cores);
-    // fprintf( stderr, "This system has %d processors configured and %d processors available.\n", get_nprocs_conf(), get_nprocs());
 
-    int i;
-    
-    // 得到总的文件大小，好一次性分配内存，此法糙快猛，嘿嘿
-    long total_size = 0;
-    for(i = 1; i < argc; i++){
-        FILE *fp = fopen(argv[i], "rb");
-        if (fp == NULL) {
-            printf("pzip: cannot open file\n");
+    // 线程池子数量取决于核数与待压缩文件数量
+    int num_thread = 1;
+    if ((argc - 1) >= num_cores)
+    {
+        num_thread = num_cores;
+    }
+    else
+    {
+        num_thread = argc - 1;
+    }
+    for (int i = 0; i < num_thread; i++)
+    {
+        pthread_t p;
+        Pthread_create(&p, NULL, consume, argv[i]);
+    }
+
+    Mutex_lock(&m);
+    while (done == 0)
+    {
+        Cond_wait(&cond, &m);
+    }
+    Mutex_unlock(&m);
+
+    // in this stage, all input files are already zipped with *.z, just merge all and output to stdout
+    char *pTemp = "tmp.z";
+    FILE *fpTemp = fopen(pTemp, "w");
+    if (fpTemp == NULL)
+    {
+        fprintf(stderr, "pzip: cannot open file %s\n", pTemp);
+        exit(1);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        char *fromFileName = malloc(strlen(argv[i]) + 2 + 1);
+        bzero(fromFileName, strlen(argv[i]) + 2 + 1);
+        strcat(fromFileName, argv[i]);
+        strcat(fromFileName, ".z");
+        FILE *fp = fopen(fromFileName, "rb");
+        if (fp == NULL)
+        {
+            fprintf(stderr, "pzip: cannot open file %s\n", fromFileName);
             exit(1);
         }
-        fseek(fp, 0, SEEK_END);
-        long fsize = ftell(fp);
-        total_size = total_size + fsize;
+        free(fromFileName);
+
+        int count = 0;
+        while (fread(&count, sizeof(int), 1, fp) == 1)
+        {
+            int c = 0;
+            fread(&c, 1, 1, fp);
+
+            fwrite(&count, 4, 1, fpTemp);
+            fprintf(fpTemp, "%c", c);
+        }
         fclose(fp);
     }
-    
-    char *original_content = malloc(total_size + 1);
-    
-    long temp = 0;
-    for(i = 1; i < argc; i++){
-        FILE *fp = fopen(argv[i], "rb");
-        if (fp == NULL) {
-            printf("pzip: cannot open file\n");
+    fclose(fpTemp);
+
+    FILE *fp = fopen(pTemp, "rb");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "pzip: cannot open file %s\n", pTemp);
+        exit(1);
+    }
+    int count = 0;
+    int last_c = 0;
+    int last_count = 0;
+    while (fread(&count, sizeof(int), 1, fp) == 1)
+    {
+        int c = 0;
+        fread(&c, 1, 1, fp);
+
+        if (last_count == 0)
+        {
+            last_c = c;
+            last_count = last_count + count;
+        }
+        else
+        {
+            if (c == last_c)
+            {
+                last_count = last_count + count;
+            }
+            else
+            {
+                fwrite(&last_count, 4, 1, stdout);
+                fprintf(stdout, "%c", last_c);
+                last_c = c;
+                last_count = count;
+            }
+        }
+    }
+    if (last_count > 0)
+    {
+        fwrite(&last_count, 4, 1, stdout);
+        fprintf(stdout, "%c", last_c);
+    }
+    fclose(fp);
+
+    exit(0);
+}
+
+void *consume(void *arg)
+{
+    while (1)
+    {
+        int k = 0;
+        Mutex_lock(&m);
+        count++;
+        k = count;
+        Mutex_unlock(&m);
+
+        if (k > file_count)
+        {
+            break;
+        }
+
+        char *fileName = pArgv[k];
+        FILE *fp = fopen(fileName, "rb");
+        if (fp == NULL)
+        {
+            fprintf(stderr, "pzip: cannot open file %s\n", fileName);
             exit(1);
         }
-        
+
+        char *toFileName = malloc(strlen(fileName) + 2 + 1);
+        strcat(toFileName, fileName);
+        strcat(toFileName, ".z");
+        FILE *fp1 = fopen(toFileName, "w");
+        if (fp1 == NULL)
+        {
+            fprintf(stderr, "pzip: cannot open file %s\n", toFileName);
+            exit(1);
+        }
+        free(toFileName);
+
         fseek(fp, 0, SEEK_END);
         long fsize = ftell(fp);
         fseek(fp, 0, SEEK_SET);
@@ -76,24 +210,21 @@ main(int argc, char *argv[])
         char *file_content = malloc(fsize + 1);
         fread(file_content, 1, fsize, fp);
         fclose(fp);
-        
-        strncpy(original_content + temp, file_content, fsize);
-        temp = temp + fsize;
-        
+
+        pzip(file_content, fsize, fp1);
+
+        fclose(fp1);
+
         free(file_content);
+
+        Mutex_lock(&m);
+        if (k >= file_count)
+        {
+            done = 1;
+        }
+        Cond_signal(&cond);
+        Mutex_unlock(&m);
     }
-    
-    original_content[total_size] = 0;
-    wzip(original_content, total_size);
-    
-    free(original_content);
-    
-    clock_t toc = clock();
-    fprintf( stderr, "Elapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
 
-    exit(0);
+    return NULL;
 }
-
-
-// gcc -o pzip pzip.c -Wall -Werror
-// gcc -o opzip pzip.c -Wall -Werror -O
--- concurrency-pzip/pzip2.c
+++ concurrency-pzip/pzip2.c
@@ -3,129 +3,72 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include "common_threads.h"
-#include "io_helper.h"
-#include <pthread.h>
 
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
-
-void *consume(void *arg);
-
-int file_count = 0;
-int count = 0;
-int done = 0;
-char **pArgv = NULL;
-
-void pzip(char *c, long size, FILE *fp)
+void
+wzip(char * c, long size)
 {
+    // printf("%s\n", c);
+    
     int i;
     int per_count = 1;
-    for (i = 0; i < size; i++)
-    {
-        if (i == (size - 1))
-        {
-            fwrite(&per_count, 4, 1, fp);
-            fprintf(fp, "%c", c[i]);
+    for(i = 0; i < size; i++){
+        if(i == (size-1)){
+            fwrite(&per_count, 4, 1, stdout);
+            printf("%c", c[i]);
             break;
         }
-
-        if (c[i] != c[i + 1])
-        {
-            fwrite(&per_count, 4, 1, fp);
-            fprintf(fp, "%c", c[i]);
+    
+        if(c[i] != c[i+1]){
+            // printf("%d%c", per_count, c[i]);
+            fwrite(&per_count, 4, 1, stdout);
+            printf("%c", c[i]);
             per_count = 1;
-        }
-        else
-        {
+        } else {
             per_count++;
         }
     }
 }
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-    pArgv = argv;
-
-    if (argc <= 1)
-    {
+    if(argc <= 1){
         printf("pzip: file1 [file2 ...]\n");
         exit(1);
     }
-
-    file_count = argc - 1;
-    count = 0;
-
+    
     clock_t tic = clock();
 
     int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
-    fprintf(stderr, "This system has %d processors\n", num_cores);
+    fprintf( stderr, "This system has %d processors\n", num_cores);
+    // fprintf( stderr, "This system has %d processors configured and %d processors available.\n", get_nprocs_conf(), get_nprocs());
 
-    // 线程池子数量取决于核数与待压缩文件数量
-    int num_thread = 1;
-    if ((argc - 1) >= num_cores)
-    {
-        num_thread = num_cores;
-    }
-    else
-    {
-        num_thread = argc - 1;
-    }
-    fprintf(stderr, "thread pool size %d\n", num_thread);
-    for (int i = 0; i < num_thread; i++)
-    {
-        pthread_t p;
-        Pthread_create(&p, NULL, consume, argv[i]);
-    }
-
-    Mutex_lock(&m);
-    while (done == 0)
-    {
-        Cond_wait(&cond, &m);
-    }
-    Mutex_unlock(&m);
-
-    clock_t toc = clock();
-    fprintf(stderr, "Elapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
-
-    exit(0);
-}
-
-void *consume(void *arg)
-{
-    while (1)
-    {
-        int k = 0;
-        Mutex_lock(&m);
-        count++;
-        k = count;
-        Mutex_unlock(&m);
-
-        if(k > file_count){
-            break;
-        }
-
-        char *fileName = pArgv[k];
-        printf("\n####thread %li %s\n", (unsigned long int)pthread_self(), fileName);
-
-        FILE *fp = fopen(fileName, "rb");
-        if (fp == NULL)
-        {
-            fprintf(stderr, "pzip: cannot open file %s\n", fileName);
+    int i;
+    
+    // 得到总的文件大小，好一次性分配内存，此法糙快猛，嘿嘿
+    long total_size = 0;
+    for(i = 1; i < argc; i++){
+        FILE *fp = fopen(argv[i], "rb");
+        if (fp == NULL) {
+            printf("pzip: cannot open file\n");
             exit(1);
         }
-
-        char *toFileName = malloc(strlen(fileName) + 2 + 1);
-        strcat(toFileName, fileName);
-        strcat(toFileName, ".z");
-        FILE *fp1 = fopen(toFileName, "w");
-        if (fp1 == NULL)
-        {
-            fprintf(stderr, "pzip: cannot open file %s\n", toFileName);
+        fseek(fp, 0, SEEK_END);
+        long fsize = ftell(fp);
+        total_size = total_size + fsize;
+        fclose(fp);
+    }
+    
+    char *original_content = malloc(total_size + 1);
+    
+    long temp = 0;
+    for(i = 1; i < argc; i++){
+        FILE *fp = fopen(argv[i], "rb");
+        if (fp == NULL) {
+            printf("pzip: cannot open file\n");
             exit(1);
         }
-        free(toFileName);
-
+        
         fseek(fp, 0, SEEK_END);
         long fsize = ftell(fp);
         fseek(fp, 0, SEEK_SET);
@@ -133,20 +76,24 @@ void *consume(void *arg)
         char *file_content = malloc(fsize + 1);
         fread(file_content, 1, fsize, fp);
         fclose(fp);
-
-        pzip(file_content, fsize, fp1);
-
-        fclose(fp1);
-
+        
+        strncpy(original_content + temp, file_content, fsize);
+        temp = temp + fsize;
+        
         free(file_content);
-
-        Mutex_lock(&m);
-        if(k >= file_count){
-            done = 1;
-        }
-        Cond_signal(&cond);
-        Mutex_unlock(&m);
     }
+    
+    original_content[total_size] = 0;
+    wzip(original_content, total_size);
+    
+    free(original_content);
+    
+    clock_t toc = clock();
+    fprintf( stderr, "Elapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
 
-    return NULL;
+    exit(0);
 }
+
+
+// gcc -o pzip pzip.c -Wall -Werror
+// gcc -o opzip pzip.c -Wall -Werror -O
